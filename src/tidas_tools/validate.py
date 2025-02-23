f3156f480eecb3a1df744806ea861ced36c650d4
Nan <linanenv@gmail.com>
2025-02-23 15:45:40 +0800

    Add logging to category validation and improve error messages

--- src/tidas_tools/validate.py
+++ src/tidas_tools/validate.py
@@ -1,11 +1,19 @@
 import importlib.resources as pkg_resources
 import json
 import os
+import logging
 
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
 import tidas_tools.schemas as schemas
+
+logging.basicConfig(
+    filename="validate.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
 
 GREEN = "\033[92m"
 RED = "\033[91m"
@@ -26,6 +34,8 @@ def category_validate(json_file_path: str, category: str):
                         validate(instance=json_item, schema=schema)
 
                     except ValidationError as e:
-                        print(f"{RED}Error: {full_path} {e.message}{RESET}")
+                        print(f"{RED}ERROR: {full_path} {e.message}{RESET}")
+                        logging.error(f"ERROR: {full_path} {e.message}")
                     else:
-                        print(f"{GREEN}Passed: {full_path}{RESET}")
+                        print(f"{GREEN}INFO: {full_path}{RESET}")
+                        logging.info(f"INFO: {full_path}")
