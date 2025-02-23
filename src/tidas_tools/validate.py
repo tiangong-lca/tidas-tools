ac2b9434377e13192477cb5624e3625fe0f816c0
Nan <linanenv@gmail.com>
2025-02-23 15:18:10 +0800

    Refactor category validation to process JSON files from a specified directory and enhance error reporting with color-coded output

--- src/tidas_tools/validate.py
+++ src/tidas_tools/validate.py
@@ -1,24 +1,31 @@
 import importlib.resources as pkg_resources
 import json
+import os
 
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
 import tidas_tools.schemas as schemas
 
+GREEN = "\033[92m"
+RED = "\033[91m"
+RESET = "\033[0m"
 
-def category_validate(json_data: list[dict], category: str):
+
+def category_validate(json_file_path: str, category: str):
+
     with pkg_resources.open_text(schemas, f"tidas_{category.lower()}.json") as f:
         schema = json.load(f)
 
-    for json_item in json_data:
-        # Use "filename" key if available, otherwise use a default label.
-        file_name = json_item.get("filename", "unknown_file")
-        try:
-            validate(instance=json_item, schema=schema)
-        except ValidationError as e:
-            log_entry = {"filename": file_name, "status": "Error", "message": e.message}
-            print(json.dumps(log_entry, indent=4, ensure_ascii=False))
-        else:
-            log_entry = {"filename": file_name, "status": "Success"}
-            print(json.dumps(log_entry, indent=4, ensure_ascii=False))
+        for filename in os.listdir(json_file_path):
+            if filename.endswith(".json"):
+                full_path = os.path.join(json_file_path, filename)
+                with open(full_path, "r") as json_file:
+                    json_item = json.load(json_file)
+                    try:
+                        validate(instance=json_item, schema=schema)
+
+                    except ValidationError as e:
+                        print(f"{RED}Error: {full_path} {e.message}{RESET}")
+                    else:
+                        print(f"{GREEN}Passed: {full_path}{RESET}")
