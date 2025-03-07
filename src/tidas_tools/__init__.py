cdab73c0763e3ac0904bf9b8571bbe66ea8dce95
Nan <linanenv@gmail.com>
2025-03-07 19:51:52 +0800

    Refactor import statements in __init__.py to use main conversion function

--- src/tidas_tools/__init__.py
+++ src/tidas_tools/__init__.py
@@ -1,4 +1,4 @@
-from .convert import convert_json_to_xml, convert_xml_to_json
+from .convert import main as convert_main
 from .validate import category_validate
 
-__all__ = ["category_validate", "convert_xml_to_json", "convert_json_to_xml"]
+__all__ = ["convert_main", "category_validate"]
